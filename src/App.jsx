45d6d76fa18898ffa4999a1939c00f1ab6a68fc4
Darshan <darshanjariwala090@gmail.com>
2025-07-02 18:58:30 +0530

    User authentication

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,7 @@ const router = createBrowserRouter([
         element: <Postjob />
       },
       {
-        path:'/saved',
+        path:'/saved-jobs',
         element: <SavedJob />
       },
     ]
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,24 +1,72 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Link, useSearchParams } from 'react-router-dom'
 import { Button } from './ui/button'
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
+import { SignedIn, SignedOut, SignIn, SignInButton, UserButton } from '@clerk/clerk-react';
+import { BriefcaseBusiness, Heart, PenBox } from 'lucide-react';
 
 const Header = () => {
+  
+  const [show,setShow] = useState(false);
+  const [search,setSearch] = useSearchParams()
+  useEffect(()=>{
+    if(search.get(('sign-in'))){
+        setShow(true)
+    }
+  },[search])
+  const handleOverlayClick = (e) => {
+    if(e.target === e.currentTarget){
+        setShow(false)
+        setSearch({})
+    }
+  }
+
   return (
     <>
         <nav className='py-4 flex justify-between items-center'>
             <Link>
                 <img src="/logo (2).png" alt="" className='h-20' />
             </Link>
-
-            <Button variant="outline">Login</Button>
-            {/* <SignedOut>
-                <SignInButton />
+            <div className='flex gap-8'>
+            <SignedOut>
+                <Button variant="outline" onClick = {()=>setShow(true)}>Login</Button>
                     </SignedOut>
                 <SignedIn>
-                <UserButton />
-            </SignedIn> */}
+                <Link to='/post-job'>
+                {/* add a condition here */}
+                    <Button variant='destructive' className='rounded-full'>
+                        <PenBox size={20} className='mr-2'/>
+                        Post a job
+                    </Button>
+                </Link>
+                <UserButton appearance={{
+                    elements:{
+                        avatarBox:'w-10 h-10'
+                    }
+                }}>
+                    <UserButton.MenuItems>
+                        <UserButton.Link
+                            label='My-jobs'
+                            labelIcon = {<BriefcaseBusiness size={15}/>}
+                            href='/my-jobs'
+                        />
+                        <UserButton.Link
+                            label='Saved-jobs'
+                            labelIcon = {<Heart size={15}/>}
+                            href='/saved-jobs'
+                        />
+                    </UserButton.MenuItems>
+                </UserButton>
+            </SignedIn>
+            </div>
         </nav>
+        {show && <div className='fixed inset-0 flex items-center justify-center bg-black/50'
+        onClick={handleOverlayClick}
+        >
+            <SignIn 
+                signUpFallbackRedirectUrl='/onboarding'
+                fallbackRedirectUrl='/onboarding'
+            />  
+        </div>}
     </>
   )
 }
